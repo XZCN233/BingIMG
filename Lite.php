85c0daa31e6d51ac25a24412016cec24a215abda
追梦工坊-中国的小赵 <82950724+XZCN233@users.noreply.github.com>
2022-04-30 09:40:19 +0800

    无注释版源码PHP without comments

---
+++ Lite.php
@@ -0,0 +1,17 @@
+<?php
+$path = "test";
+$filename = $path . "/" . date("Ymd") . ".jpg";
+$api = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'); 
+$api = json_decode($api,true);
+$url = 'https://cn.bing.com'.$api['images'][0]['url'];
+if(!file_exists($path)){mkdir($path, 0777);}
+if(!file_exists($filename)){
+    if(file_put_contents($filename,file_get_contents($url))){
+        echo '{"Return":"200","Location":"'. $filename . '";}';
+    }else {
+        echo '{"Return":"Err","Reason":"Can-Not-Download";}';
+    }
+}else {
+    echo '{"Return":"Err","Reason":"Already-Downloaded";}';
+}
+?>
